547c9aa0a4d64c7a5242b081046ceecac1e14420
Corvin Zander <64727944+CorKnight@users.noreply.github.com>
2024-01-01 20:15:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from werkzeug.contrib.fixers import ProxyFix
 from clothingStruct import getWashingInstructionsForClothes
 from washerStruct import createWasherDictByClothes, flattenDictAtColors
 
@@ -28,5 +27,4 @@ def washing():
 
 
 if __name__ == '__main__':
-    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run()
